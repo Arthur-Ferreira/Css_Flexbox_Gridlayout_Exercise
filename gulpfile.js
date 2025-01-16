2762ad2fb8df4a6faf91a921d2f68c336b48e69c
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-01-16 23:49:59 +0000

    Feat New Version: Bugfix at gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 
 // Sass Task
 gulp.task('sass', function () {
-    return gulp.src('src/scss/*.scss')
+    return gulp.src('src/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('build/css'))
 });
