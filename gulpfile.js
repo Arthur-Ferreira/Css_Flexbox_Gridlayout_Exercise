1212250abf9a89c0cae7487c509efb4af097aa75
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-01-20 06:52:53 +0000

    Feat New Version: Bugfix at Image minification

--- gulpfile.js
+++ gulpfile.js
@@ -9,30 +9,33 @@ gulp.task('htmlmin', function () {
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('build'))
         .pipe(browserSync.stream());
-});
-
-// Sass Task
-gulp.task('sass', function () {
-    return gulp.src('src/styles/*.scss')
+    });
+    
+    // Sass Task
+    gulp.task('sass', function () {
+        return gulp.src('src/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('build/styles'))
         .pipe(browserSync.stream());
-});
-
-// Image Optimization Task
-gulp.task('image', function () {
-    return gulp.src('src/images/*')
+    });
+    
+    // Image Optimization Task
+    gulp.task('image', async function () {
+        const imagemin = await import ('gulp-imagemin');
+        return gulp.src('src/images/*', { encoding: false})
+        .pipe(imagemin.default())
         .pipe(gulp.dest('build/images'))
-});
-
-// BrowserSync Task
-gulp.task('serve', function () {
-    browserSync.init({
+        .pipe(browserSync.stream());
+    });
+    
+    // BrowserSync Task
+    gulp.task('serve', function () {
+        browserSync.init({
         server: {
             baseDir: './build'
         }
     });
-
+    
     gulp.watch('src/**/*.html', gulp.series('htmlmin')).on('change', browserSync.reload);
     gulp.watch('src/styles/*.scss', gulp.series('sass')).on('change', browserSync.reload);
     gulp.watch('src/images/*', gulp.series('image')).on('change', browserSync.reload);
