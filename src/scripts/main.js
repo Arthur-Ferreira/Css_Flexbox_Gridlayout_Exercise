000fddd749baf3b093c83133d74897e64558233d
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-01-20 19:44:35 +0000

    Feat New Version: main.js update to handle every button event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,64 @@
-const fragment = './views/second.html';
+// Main Section
 const mainFirst = document.querySelector('#first');
-const testButton = document.getElementById('test');
 
-testButton.addEventListener('click', () => {
-    fetch(fragment)
-    .then(response => {
-        if(!response.ok){
-            throw new Error('Error fetching the file');
-        }
-        return response.text();
-    })
-    .then(html => {
-        mainFirst.innerHTML = html;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-})
+// Fragment Section
+const secondFragment = './views/second.html';
+const thirdFragment = './views/third.html';
+const forthFragment = './views/forth.html';
+
+// Action Button
+const secondFragmentButton = document.getElementById('secondFragment');
+const thirdFragmentButton = document.getElementById('thirdFragment');
+const forthFragmentButton = document.getElementById('forthFragment');
+
+
+secondFragmentButton.addEventListener('click', () => {
+    fetch(secondFragment)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error fetching the file');
+            }
+            return response.text();
+        })
+        .then(html => {
+            mainFirst.innerHTML = html;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+
+thirdFragmentButton.addEventListener('click', () => {
+    fetch(thirdFragment)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error fetching the file');
+            }
+            return response.text();
+        })
+        .then(html => {
+            mainFirst.innerHTML = html;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+
+forthFragmentButton.addEventListener('click', () => {
+    fetch(forthFragment)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error fetching the file');
+            }
+            return response.text();
+        })
+        .then(html => {
+            mainFirst.innerHTML = html;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
 
